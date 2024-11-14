809542431ef63a8a83c570cdc6723c5afdb44637
Da-hwon Ju <95617014+hwonda@users.noreply.github.com>
2024-11-14 22:29:51 +0900

    [형식] : 전체 레이아웃을 수정한다 (#19)
    * [형식] : 글자 스타일 조정한다
    
    * [형식] : Footer 추가한다
    
    * [형식] : Header의 layout(애니메이션 등)을 변경한다
    
    * [기능] : 사용자의 스크롤 이벤트에 따라 헤더를 숨기고 표출한다
    
    * [버그] : system 테마 설정 시, 관련도 삼각형의 색상을 알맞게 수정한다
    
    * [형식] : section에 margin을 추가한다

--- src/components/server/common/Header.tsx
+++ src/components/server/common/Header.tsx
@@ -1,46 +1,28 @@
-import dynamic from 'next/dynamic';
 import Link from 'next/link';
-
-// ThemeSwitch를 클라이언트 컴포넌트로 동적 로딩
-const ThemeSwitch = dynamic(
-  () => import('@/components/client/theme/ThemeSwitch'),
-  { ssr: false }
-);
-
-// const CollapseMenu = dynamic(
-//   () => import('@/components/client/common/CollapseMenu'),
-//   { ssr: false }
-// );
+// import { Search } from 'lucide-react';
+import ScrollDirectionHandler from '@/components/client/common/ScrollDirectionHandler';
+import LogoAnimation from '@/components/client/common/LogoAnimation';
+import ThemeSwitch from '@/components/client/theme/ThemeSwitch';
 
 const Header = () => {
-  // const navItems = [
-  //   { href: '/1', label: '메뉴1' },
-  //   { href: '/2', label: '메뉴2' },
-  // ];
-
   return (
-    <header className='fixed left-0 top-0 z-50 w-full border-b border-secondary bg-background opacity-100 sm:opacity-80 hover:opacity-100'>
-      <div className='mx-auto flex justify-between sm:grid sm:grid-cols-2 max-w-6xl items-center p-3 md:px-6 lg:px-8'>
-        <Link href='/'>
-          <h1 className='text-3xl font-bold font-noto text-primary'>{'Diki'}</h1>
-        </Link>
-        {/* <div className='hidden sm:flex justify-center'>
-          {navItems.map((item) => (
-            <Link key={item.href} href={item.href} className='mx-3'>
-              {item.label}
+    <>
+      <ScrollDirectionHandler />
+      <header className='fixed left-0 top-0 z-50 w-full bg-background transition-transform duration-1000 ease-in-out' style={{ transform: 'translateY(var(--header-transform, 0))' }}>
+        <div className='mx-auto flex justify-center max-w-6xl items-center p-3 md:px-6 lg:px-8'>
+          <div className='w-full flex justify-end gap-3 items-center'>
+            <Link href='/posts'>
+              <LogoAnimation />
             </Link>
-          ))}
-        </div> */}
-        <div className='flex justify-end items-center gap-2'>
-          <div>
+            {/* 검색버튼 */}
+            {/* <button className='flex rounded-md p-2 hover:bg-background-secondary duration-300'>
+              <Search className='size-4' />
+            </button> */}
             <ThemeSwitch />
           </div>
-          {/* <div className='block sm:hidden'>
-            <CollapseMenu navItems={navItems} />
-          </div> */}
         </div>
-      </div>
-    </header>
+      </header>
+    </>
   );
 };
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -30,7 +30,7 @@ const config: Config = {
           css: {
             maxWidth: 'none',
             'h1, h2, h3, strong': {
-              marginBottom: '1rem',
+              marginBottom: '0.5rem',
               color: 'var(--text)',
             },
             'p, span, li': {
@@ -45,8 +45,9 @@ const config: Config = {
               marginBottom: '0.75rem',
               lineHeight: '1.7',
             },
-            strong: {
-              padding: '0.1rem',
+            section: {
+              marginTop: '6rem',
+              marginBottom: '6rem',
             },
 
             '.prose :where(h1):not(:where([class~="not-prose"],[class~="not-prose"] *))': {
@@ -54,10 +55,10 @@ const config: Config = {
               fontFamily: 'var(--font-noto)',
             },
             '.prose :where(h2):not(:where([class~="not-prose"],[class~="not-prose"] *))': {
-              marginTop: '6rem',
+              marginTop: '0',
               fontSize: '1.2rem',
               fontWeight: 'semibold',
-              fontFamily: 'var(--font-noto)',
+              // fontFamily: 'var(--font-noto)',
             },
             '.prose :where(h3):not(:where([class~="not-prose"],[class~="not-prose"] *))': {
               fontSize: '1.05rem',
