401fe934ae12136a1d7565a495e8ffd4a1bb9d38
judahwon <hwondaa@gmail.com>
2025-01-11 21:50:58 +0900

    [형식] : prev-next section 호버 시 background color 부여한다

--- src/components/posts/sections/PrevNextSection.tsx
+++ src/components/posts/sections/PrevNextSection.tsx
@@ -25,10 +25,10 @@ const PrevNextSection = async ({ id, lastTermId }: Props) => {
   return (
     <div className="grid grid-cols-2 my-[-16px]">
       {prevTerm && (
-        <Link href={`${ prevTerm.url }`} className='group min-h-20 flex flex-col justify-center col-start-1 border border-secondary border-r-0 p-2 hover:no-underline rounded-l-md'>
-          <div className='flex items-center gap-1'>
-            <ChevronLeft className='size-4 sm:size-5 text-primary' />
-            <span className='text-xs sm:text-sm text-primary'>{'이전 포스트'}</span>
+        <Link href={`${ prevTerm.url }`} className='group min-h-20 flex flex-col justify-center col-start-1 border border-secondary border-r-0 p-2 hover:no-underline rounded-l-md hover:bg-background-secondary'>
+          <div className='flex items-center gap-1 font-normal'>
+            <ChevronLeft className='size-4 sm:size-5 text-sub group-hover:text-primary' />
+            <span className='text-xs sm:text-sm text-sub group-hover:text-primary'>{'이전 포스트'}</span>
           </div>
           <div className='flex items-center gap-1 mt-1 ml-2 text-sm sm:text-base text-sub pb-1'>
             <span className='font-semibold text-sub group-hover:text-primary underline underline-offset-4 group-hover:decoration-primary'>
@@ -39,10 +39,10 @@ const PrevNextSection = async ({ id, lastTermId }: Props) => {
         </Link>
       )}
       {nextTerm && (
-        <Link href={`${ nextTerm.url }`} className='group min-h-20 col-start-2 flex flex-col justify-center items-end border border-secondary p-2 hover:no-underline rounded-r-md'>
-          <div className='flex items-center gap-1'>
-            <span className='text-xs sm:text-sm text-primary'>{'다음 포스트'}</span>
-            <ChevronRight className='size-4 sm:size-5 text-primary' />
+        <Link href={`${ nextTerm.url }`} className='group min-h-20 col-start-2 flex flex-col justify-center items-end border border-secondary p-2 hover:no-underline rounded-r-md hover:bg-background-secondary'>
+          <div className='flex items-center gap-1 font-normal'>
+            <span className='text-xs sm:text-sm text-sub group-hover:text-primary'>{'다음 포스트'}</span>
+            <ChevronRight className='size-4 sm:size-5 text-sub group-hover:text-primary' />
           </div>
           <div className='flex justify-end items-center gap-1 mt-1 mr-1.5 text-primary text-sm sm:text-base pb-1'>
             <span className='font-semibold text-sub group-hover:text-primary underline underline-offset-4 group-hover:decoration-primary'>
