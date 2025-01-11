fb6bbc8ccc0c7e8cd1ef32b266f99edfcfce17e9
judahwon <hwondaa@gmail.com>
2025-01-11 21:18:32 +0900

    [형식] : 추천포스트 좌우 padding 수정한다

--- src/components/posts/RecommendTerms.tsx
+++ src/components/posts/RecommendTerms.tsx
@@ -61,22 +61,22 @@ export default function RecommendTerms() {
   }, [recentTerms]);
 
   return (
-    <div className='space-y-1.5'>
+    <div className='w-full space-y-1.5'>
       <div className='flex items-center gap-0.5 sm:gap-1'>
         <Rocket className='size-4' />
         <h3 className='text-base text-sub font-semibold'>{'최신 포스트'}</h3>
       </div>
-      <div ref={containerRef} className='flex justify-between gap-2 overflow-hidden'>
+      <div ref={containerRef} className='flex justify-between gap-1.5 overflow-hidden'>
         {visibleItems.map((term) => (
           <Link
             key={term.url}
             href={`${ term.url }`}
-            className='py-1.5 flex justify-center items-center text-sub rounded-lg border border-light hover:border-primary hover:text-primary hover:font-semibold transition-colors text-xs sm:text-sm shrink-0 bg-background'
             style={{ width: 'var(--item-width)' }}
+            className='py-1.5 px-2.5 flex justify-center items-center text-sub rounded-lg border border-light hover:border-primary hover:text-primary hover:font-semibold transition-colors text-xs md:text-sm shrink-0 bg-background'
           >
-            {(term.title?.ko ?? '').length > 8
-              ? `${ term.title?.ko?.slice(0, 8) }...`
-              : term.title?.ko}
+            <span className="overflow-hidden text-nowrap text-ellipsis">
+              {term.title?.ko}
+            </span>
           </Link>
         ))}
       </div>
