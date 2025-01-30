71eb28370262064be5acd0d131291d05be6df811
judahwon <hwondaa@gmail.com>
2025-01-30 16:45:03 +0900

    [버그] : google-site-verification Id값 수정한다

--- src/components/meta/GoogleVerification.tsx
+++ src/components/meta/GoogleVerification.tsx
@@ -1,5 +1,5 @@
 const GoogleVerification = () => {
-  const siteVerificationId = '34dkEhDhush7JO9qT1TlWowPtkolmsc5wDu9Vr_qLQ';
+  const siteVerificationId = 'D34dkEhDhush7JO9qT1TlWowPtkolmsc5wDu9Vr_qLQ';
 
   return(
     <meta
