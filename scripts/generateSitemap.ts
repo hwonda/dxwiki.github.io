258bd54b03b722f416b1867d4108e97cd25edc3d
judahwon <hwondaa@gmail.com>
2025-02-04 23:00:58 +0900

    [버그] : sitemap.xml 루트,public에 추가한다

--- scripts/generateSitemap.ts
+++ scripts/generateSitemap.ts
@@ -5,7 +5,10 @@ import { getSitemapURLs, generateSitemapXML } from '../src/utils/sitemap';
   const urls = await getSitemapURLs();
   const sitemap = generateSitemapXML(urls);
 
-  // 사이트맵 파일 생성
-  await fs.writeFile('public/sitemap.xml', sitemap);
-  console.log('sitemap.xml generated', sitemap);
+  // 사이트맵 파일 생성 (public 폴더와 루트 디렉토리)
+  await Promise.all([
+    fs.writeFile('public/sitemap.xml', sitemap),
+    fs.writeFile('sitemap.xml', sitemap),
+  ]);
+  console.log('sitemap.xml generated in public/ and root directory');
 })();
--- src/utils/sitemap.ts
+++ src/utils/sitemap.ts
@@ -34,7 +34,7 @@ const generateSitemapURL = ({ loc, lastmod, changefreq, priority }: SitemapURL):
 
 const generateSitemapXML = (urls: SitemapURL[]): string => {
   return `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+    <urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
       ${ urls.map(generateSitemapURL).join('') }
     </urlset>`;
 };
