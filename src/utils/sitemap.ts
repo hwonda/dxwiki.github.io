314e53a2b56a54cc9c7180ae8bf9c25211b936f3
judahwon <hwondaa@gmail.com>
2025-02-18 09:11:44 +0900

    [버그] : sitemap encodeURL 제거한다

--- src/utils/sitemap.ts
+++ src/utils/sitemap.ts
@@ -8,23 +8,10 @@ interface SitemapURL {
   priority: number;
 }
 
-const encodeURL = (url: string): string => {
-  return url.replace(/[&<>"']/g, (char) => {
-    const entities: Record<string, string> = {
-      '&': '&amp;',
-      '<': '&lt;',
-      '>': '&gt;',
-      '"': '&quot;',
-      '\'': '&apos;',
-    };
-    return entities[char];
-  });
-};
-
 const generateSitemapURL = ({ loc, lastmod, changefreq, priority }: SitemapURL): string => {
   return `
     <url>
-      <loc>${ encodeURL(loc) }</loc>
+      <loc>${ loc }</loc>
       <lastmod>${ lastmod }</lastmod>
       <changefreq>${ changefreq }</changefreq>
       <priority>${ priority }</priority>
@@ -34,7 +21,7 @@ const generateSitemapURL = ({ loc, lastmod, changefreq, priority }: SitemapURL):
 
 const generateSitemapXML = (urls: SitemapURL[]): string => {
   return `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
       ${ urls.map(generateSitemapURL).join('') }
     </urlset>`;
 };
