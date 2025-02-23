1a93a972da576b88443d8264854e7cd3c3d3b83d
judahwon <hwondaa@gmail.com>
2025-02-23 18:00:24 +0900

    [버그] : static public -> out 저장경로 수정한다

--- scripts/generateSitemap.ts
+++ scripts/generateSitemap.ts
@@ -7,7 +7,7 @@ import { getSitemapURLs, generateSitemapXML } from '../src/utils/sitemap';
 
   // 사이트맵 파일 생성 (public 폴더와 루트 디렉토리)
   await Promise.all([
-    fs.writeFile('public/sitemap.xml', sitemap),
+    fs.writeFile('out/sitemap.xml', sitemap),
   ]);
   console.log('sitemap.xml generated in public/ and root directory');
 })();
