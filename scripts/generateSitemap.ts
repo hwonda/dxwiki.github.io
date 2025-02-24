d442ec821534250decb8a6ed631abec4e1c8a3ae
judahwon <hwondaa@gmail.com>
2025-02-25 08:27:08 +0900

    [형식] : sitemap-temp2 추가한다

--- scripts/generateSitemap.ts
+++ scripts/generateSitemap.ts
@@ -1,4 +1,4 @@
-import { promises as fs } from 'fs';
+import { writeFileSync } from 'fs';
 import { getSitemapURLs, generateSitemapXML } from '../src/utils/sitemap';
 
 (async () => {
@@ -7,7 +7,8 @@ import { getSitemapURLs, generateSitemapXML } from '../src/utils/sitemap';
 
   // 사이트맵 파일 생성 (public 폴더와 루트 디렉토리)
   await Promise.all([
-    fs.writeFile('out/sitemap.xml', sitemap),
+    writeFileSync('out/sitemap.xml', sitemap, 'utf-8'),
+    writeFileSync('out/sitemap-temp2.xml', sitemap, 'utf-8'),
   ]);
   console.log('sitemap.xml generated in public/ and root directory');
 })();
