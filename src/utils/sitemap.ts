76d714f376c65bf567f6dde4adfaa142429529ce
judahwon <hwondaa@gmail.com>
2025-03-03 22:06:50 +0900

    [테스트] : sitemap-escape 전체 코드로 변경한다

--- src/utils/sitemap.ts
+++ src/utils/sitemap.ts
@@ -43,17 +43,17 @@ const generateSitemapURL = ({ loc, lastmod, changefreq, priority }: SitemapURL):
 };
 
 const generateSitemapByEscapeXML = ({ urls }: { urls: SitemapURL[] }): string => {
-  return `<?xml version="1.0" encoding="UTF-8"?>
+  return escapeXML(`<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
     ${ urls.map(({ loc, lastmod, changefreq, priority }) => `
       <url>
-        <loc>${ escapeXML(loc) }</loc>
+        <loc>${ loc }</loc>
         <lastmod>${ lastmod }</lastmod>
         <changefreq>${ changefreq }</changefreq>
         <priority>${ priority }</priority>
       </url>
     `).join('') }
-  </urlset>`;
+  </urlset>`);
 };
 
 const generateSitemapByEncodeXML = ({ urls }: { urls: SitemapURL[] }): string => {
