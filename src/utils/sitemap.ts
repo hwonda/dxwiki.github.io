0c3748f1f7bf45277121d2fc325ec88f7ab9da8c
hwonda <hwondaa@gmail.com>
2025-03-10 23:14:42 +0000

    [형식] : 불필요한 sitemap 형식 제거한다

--- src/utils/sitemap.ts
+++ src/utils/sitemap.ts
@@ -4,70 +4,15 @@ import { fetchTermsData } from '@/utils/fetchData';
 interface SitemapURL {
   loc: string;
   lastmod: string;
-  changefreq: string;
-  priority: number;
 }
 
-const escapeXML = (str: string): string => {
-  return str
-    .replace(/&/g, '&amp;')
-    .replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;')
-    .replace(/"/g, '&quot;')
-    .replace(/'/g, '&apos;');
-};
-
-const encodeURL = (url: string): string => {
-  return url.replace(/[&<>"'*]/g, (char) => {
-    const entities: Record<string, string> = {
-      '&': '&amp;',
-      '<': '&lt;',
-      '>': '&gt;',
-      '"': '&quot;',
-      '\'': '&apos;',
-    };
-
-    return entities[char] || char;
-  });
-};
-
-const generateSitemapURL = ({ loc, lastmod, changefreq, priority }: SitemapURL): string => {
+const generateSitemapURL = ({ loc, lastmod }: SitemapURL): string => {
   return `
     <url>
       <loc>${ loc }</loc>
       <lastmod>${ lastmod }</lastmod>
-      <changefreq>${ changefreq }</changefreq>
-      <priority>${ priority }</priority>
     </url>
   `;
-};
-
-const generateSitemapByEscapeXML = ({ urls }: { urls: SitemapURL[] }): string => {
-  return escapeXML(`<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
-    ${ urls.map(({ loc, lastmod, changefreq, priority }) => `
-      <url>
-        <loc>${ loc }</loc>
-        <lastmod>${ lastmod }</lastmod>
-        <changefreq>${ changefreq }</changefreq>
-        <priority>${ priority }</priority>
-      </url>
-    `).join('') }
-  </urlset>`);
-};
-
-const generateSitemapByEncodeXML = ({ urls }: { urls: SitemapURL[] }): string => {
-  return `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
-    ${ urls.map(({ loc, lastmod, changefreq, priority }) => `
-      <url>
-        <loc>${ encodeURL(loc) }</loc>
-        <lastmod>${ lastmod }</lastmod>
-        <changefreq>${ changefreq }</changefreq>
-        <priority>${ priority }</priority>
-      </url>
-    `).join('') }
-  </urlset>`;
 };
 
 const generateSitemapXML = (urls: SitemapURL[]): string => {
@@ -89,14 +34,10 @@ export const getSitemapURLs = async (): Promise<SitemapURL[]> => {
     {
       loc: baseUrl,
       lastmod: formatDate(new Date()),
-      changefreq: 'always',
-      priority: 1.00,
     },
     {
       loc: `${ baseUrl }/posts`,
       lastmod: formatDate(new Date()),
-      changefreq: 'always',
-      priority: 0.80,
     },
     ...postLists.map(({ url, metadata }) => {
       const date = metadata?.updated_at || metadata?.created_at;
@@ -105,8 +46,6 @@ export const getSitemapURLs = async (): Promise<SitemapURL[]> => {
       return {
         loc: `${ baseUrl }${ url }`,
         lastmod: formatDate(lastmod),
-        changefreq: 'always',
-        priority: 0.64,
       };
     }),
   ];
@@ -114,4 +53,4 @@ export const getSitemapURLs = async (): Promise<SitemapURL[]> => {
   return urls;
 };
 
-export { generateSitemapXML, generateSitemapByEscapeXML, generateSitemapByEncodeXML };
+export { generateSitemapXML };
